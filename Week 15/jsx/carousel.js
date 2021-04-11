f96a83589ef062eb7bcc1ebe647fb43e414c247b
Kevinabcabc <1356075038@qq.com>
2021-04-11 23:57:30 +0800

    done

---
+++ Week 15/jsx/carousel.js
@@ -0,0 +1,90 @@
+import {Component} from './framworks';
+
+export class Carousel extends Component {
+  constructor(type) {
+    super();
+    this.attributes = Object.create(null);
+  }
+  setAttribute(name, value) {
+    this.attributes[name] = value;
+    console.log(111, this.attributes);
+  }
+  render() {
+    console.log(222, this.attributes);
+    this.root = document.createElement('div');
+    this.root.classList.add("carousel");
+    for (const record of (this.attributes.src||[])) {
+      let child = document.createElement('div');
+      child.style.backgroundImage = `url('${record}')`;
+      this.root.appendChild(child);
+    }
+
+    let position = 0;
+
+    this.root.addEventListener('mousedown', event => {
+      console.log('mousedown');
+      let startX = event.clientX;
+      let children = this.root.children;
+
+      let move = event => {
+        let x = event.clientX - startX;
+
+        let current = position - ((x - x % 500) / 500);
+
+        for (const offset of [-1, 0, 1]) {
+          let pos = current + offset;
+          pos = (pos + children.length) % children.length;
+          children[pos].style.transition = 'none';
+          children[pos].style.transform = `translateX(${- pos * 500 + offset * 500 + x % 500}px)`;
+        }
+      }
+
+      let up = event => {
+        console.log('up');
+        let x = event.clientX - startX;
+        position = position - Math.round(x / 500);
+
+        for (const offset of [0, -Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
+          let pos = position + offset;
+          pos = (pos + children.length) % children.length;
+          children[pos].style.transition = '';
+          children[pos].style.transform = `translateX(${- pos * 500 + offset * 500}px)`;
+        }
+
+        document.removeEventListener('mousemove', move);
+        document.removeEventListener('mouseup', up);
+      }
+
+      document.addEventListener('mousemove', move);
+      document.addEventListener('mouseup', up);
+    });
+
+
+    // let currentIndex = 0;
+
+    // setInterval(() => {
+    //   let children = this.root.children;
+    //   let nextIndex = (currentIndex + 1) % children.length;
+
+    //   let current = children[currentIndex];
+    //   let next = children[nextIndex];
+
+    //   next.style.transition = 'none';
+    //   next.style.transform = `translateX(${100 - nextIndex * 100}%)`;
+
+    //   setTimeout(() => {
+    //     next.style.transition = '';
+    //     current.style.transform = `translateX(${-100 - nextIndex * 100}%)`;
+    //     next.style.transform = `translateX(${-nextIndex * 100}%)`;
+
+    //     currentIndex = nextIndex;
+    //   }, 16);
+    // }, 2000);
+
+    return this.root;
+  }
+  mountTo(parent) {
+    // this.render();
+    parent.appendChild(this.render());
+  }
+}
---
+++ Week 15/jsx/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./animation-demo.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [["@babel/plugin-transform-react-jsx", {pragma: "createElement"}]],
+          }
+        }
+      }
+    ]
+  },
+  mode: "development",
+}
